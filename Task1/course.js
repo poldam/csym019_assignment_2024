fb2c05ed1138d46a49878cd3a507ce4e0c57cd2e
Polyvios Damianakis <info@estros.gr>
2024-04-09 16:14:25 +0300

    Live currencies and KEY FACTS + accordion

--- Task1/course.js
+++ Task1/course.js
@@ -1,12 +1,18 @@
 var LESSONS = [];
 var SELECTEDLESSON = 0;
-var SELECTEDCURR = 'POUNDS';
+var SELECTEDCURR = 'euro';
+var USDRATE = 1.27;
+var EURORATE = 1.17;
 
 function loadAndHandleJSON(jsonFile, callback) {
     fetch(jsonFile)
         .then(response => response.json())
         .then(data => callback(null, data))
         .catch(error => callback(error, null));
+
+    setTimeout(function () {
+        loadAndHandleJSON('./course.json', setUpLessons);
+    }, 10000);
 }
 
 function setUpLessons(error, lessons) {
@@ -16,8 +22,9 @@ function setUpLessons(error, lessons) {
         console.log('JSON file loaded successfully:', lessons);
         // Get the reference to the <ul> element
         var lessonsList = document.getElementById("lessonsList");
+        lessonsList.innerHTML = '';
 
-        lessons.forEach(function(lesson) {
+        lessons.forEach(function (lesson) {
             console.log(lesson.id, lesson.title);
             LESSONS[lesson.id] = lesson;
             // Create a new <li> element
@@ -28,23 +35,19 @@ function setUpLessons(error, lessons) {
             // Set the text content of the <li> element to the current item's name
             li.textContent = lesson.title;
             // Append the <li> element to the <ul> element
-            li.addEventListener("click", lessonCLicked);
+            li.addEventListener("click", lessonClicked);
             lessonsList.appendChild(li);
         });
         // Do something with the loaded JSON data
     }
 }
 
-// Example usage:
-loadAndHandleJSON('./course.json', setUpLessons);
-
-function lessonCLicked(event) {
+function lessonClicked(event) {
     var lessonLinks = document.querySelectorAll(".lessonlink");
-    
-    lessonLinks.forEach(function(link) {
+    lessonLinks.forEach(function (link) {
         link.classList.remove("lessonlinkActive");
     });
-    
+
     event.target.classList.add("lessonlinkActive");
 
     // Extract the ID of the clicked <li> element
@@ -54,13 +57,217 @@ function lessonCLicked(event) {
 
     console.log(lesson);
 
+    var currency = "EUR";
+    var rate = EURORATE;
+    if (SELECTEDCURR == "pound") {
+        currency = "GBP";
+        rate = 1;
+    }
+    else if (SELECTEDCURR == "dollar") {
+        currency = "USD";
+        rate = USDRATE;
+    }
+
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: currency, // Change 'USD' to the appropriate currency code
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+    });
+
+    var html = "";
+
     // Display the lesson details
     var lessonDetails = document.getElementById("lessonDetails");
+    var lessonKeys = document.getElementById("lessonKeys");
+
+    lessonDetails.innerHTML = '';
+    lessonKeys.innerHTML = '';
+
     if (lesson) {
-        lessonDetails.innerHTML = "<h3>" + lesson.title + "</h3>";
-        lessonDetails.innerHTML += "<p>" + lesson.overview + "</p>";
-        lessonDetails.innerHTML += "<p><a class='button roboto-bold' target='_blank' href='" + lesson.link + "'> Read more </a></p>";
+        if (lesson.title)
+            lessonDetails.innerHTML += "<h2>" + lesson.title + "</h2>";
+        if (lesson.overview)
+            lessonDetails.innerHTML += "<p>" + lesson.overview + "</p>";
+        if (lesson.link)
+            lessonDetails.innerHTML += "<p><a class='button roboto-bold' target='_blank' href='" + lesson.link + "'> Read more </a></p>";
+
+        if (lesson.COURSECONTENT) {
+            lessonDetails.innerHTML += "<hr>";
+            lessonDetails.innerHTML += "<h3>COURSE CONTENT</h3>";
+
+            if (lesson.COURSECONTENT.CourseDetails) {
+                html = "";
+                if (lesson.COURSECONTENT.CourseDetails.html)
+                    html = lesson.COURSECONTENT.CourseDetails.html
+
+                lessonDetails.innerHTML += "<button class='accordion'>Course Details</button>";
+                lessonDetails.innerHTML += "<div class='panel'>" + html + "</div>";
+            }
+
+            if (lesson.COURSECONTENT.EntryRequirements) {
+                html = "";
+                if (lesson.COURSECONTENT.EntryRequirements.html)
+                    html = lesson.COURSECONTENT.EntryRequirements.html
+
+                lessonDetails.innerHTML += "<button class='accordion'>Entry Requirements</button>";
+                lessonDetails.innerHTML += "<div class='panel'>" + html + "</div>";
+            }
+
+            if (lesson.COURSECONTENT.FeesandFunding) {
+                html = "";
+                if (lesson.COURSECONTENT.FeesandFunding.html)
+                    html = lesson.COURSECONTENT.FeesandFunding.html
+
+                lessonDetails.innerHTML += "<button class='accordion'>Fees and Funding</button>";
+                lessonDetails.innerHTML += "<div class='panel'>" + html + "</div>";
+            }
+
+            if (lesson.COURSECONTENT.StudentPerks) {
+                html = "";
+                if (lesson.COURSECONTENT.StudentPerks.html)
+                    html = lesson.COURSECONTENT.StudentPerks.html
+                lessonDetails.innerHTML += "<button class='accordion'>Student Perks</button>";
+                lessonDetails.innerHTML += "<div class='panel'>" + html + "</div>";
+            }
+
+            if (lesson.COURSECONTENT.IntegratedFoundationYear) {
+                html = "";
+                if (lesson.COURSECONTENT.IntegratedFoundationYear.html)
+                    html = lesson.COURSECONTENT.IntegratedFoundationYear.html
+                lessonDetails.innerHTML += "<button class='accordion'>Integrated Foundation Year (IFY)</button>";
+                lessonDetails.innerHTML += "<div class='panel'>" + html + "</div>";
+            }
+
+            if (lesson.COURSECONTENT.FAQs) {
+                html = "";
+                if (lesson.COURSECONTENT.FAQs.html)
+                    html = lesson.COURSECONTENT.FAQs.html
+                lessonDetails.innerHTML += "<button class='accordion'>FAQs</button>";
+                lessonDetails.innerHTML += "<div class='panel'>" + html + "</div>";
+            }
+        }
+
+        if (lesson.KEYFACTS) {
+            lessonKeys.innerHTML += "<h2>KEY FACTS</h2>";
+            if (lesson.KEYFACTS.UCASCode) {
+                lessonKeys.innerHTML += "<div class='roboto-bold'>UCAS Code</div>";
+                lesson.KEYFACTS.UCASCode.forEach(function (item) {
+                    lessonKeys.innerHTML += "<div><span class='roboto-medium'>" + item.type + "</span>: " + item.value + "</div>";
+                });
+
+                lessonKeys.innerHTML += "<hr>";
+            }
+
+            if (lesson.KEYFACTS.Level) {
+                lessonKeys.innerHTML += "<div><span class='roboto-bold'>Level</span>: " + lesson.KEYFACTS.Level + "</div>";
+                lessonKeys.innerHTML += "<hr>";
+            }
+
+            if (lesson.KEYFACTS.Duration) {
+                lessonKeys.innerHTML += "<div class='roboto-bold'>Duration</div>";
+                lesson.KEYFACTS.Duration.forEach(function (item) {
+                    lessonKeys.innerHTML += "<div><span class='roboto-medium'>" + item.type + "</span>: " + item.value + "</div>";
+                });
+
+                lessonKeys.innerHTML += "<hr>";
+            }
+
+            if (lesson.KEYFACTS.Starting) {
+                lessonKeys.innerHTML += "<div><span class='roboto-bold'>Starting</span>: " + lesson.KEYFACTS.Starting + "</div>";
+                lessonKeys.innerHTML += "<hr>";
+            }
+
+            if (lesson.KEYFACTS.EntryRequirements) {
+                lessonKeys.innerHTML += "<div class='roboto-bold'>Entry Requirements:</div><div>" + lesson.KEYFACTS.EntryRequirements + "</div>";
+                lessonKeys.innerHTML += "<hr>";
+            }
+
+            if (lesson.KEYFACTS.Fees) {
+
+                if (lesson.KEYFACTS.Fees.UK) {
+                    lessonKeys.innerHTML += "<div class='roboto-bold'>Fees UK</div>";
+                    lesson.KEYFACTS.Fees.UK.forEach(function (item) {
+                        lessonKeys.innerHTML += "<div><span class='roboto-medium'>" + item.type + "</span>: " + formatter.format(Math.round(rate*item.value)) + "</div>";
+                    });
+                }
+
+                lessonKeys.innerHTML += "<br>";
+
+                if (lesson.KEYFACTS.Fees.International) {
+                    lessonKeys.innerHTML += "<div class='roboto-bold'>Fees International</div>";
+                    lesson.KEYFACTS.Fees.International.forEach(function (item) {
+                        lessonKeys.innerHTML += "<div><span class='roboto-medium'>" + item.type + "</span>: " + formatter.format(Math.round(rate*item.value)) + "</div>";
+                    });
+                }
+
+                lessonKeys.innerHTML += "<hr>";
+            }
+
+            if (lesson.KEYFACTS.Location) {
+                lessonKeys.innerHTML += "<div><span class='roboto-bold'>Location</span>: " + lesson.KEYFACTS.Location + "</div>";
+            }
+        }
     } else {
         lessonDetails.innerHTML = "<p>Το μάθημα δεν βρέθηκε.</p>";
     }
+
+    var acc = document.querySelectorAll(".accordion");
+
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
 }
+
+function currencyChanged(event) {
+    SELECTEDCURR = document.getElementById("currencySelector").value;
+    // console.log(SELECTEDCURR);
+    // console.log(SELECTEDLESSON);
+    if(SELECTEDLESSON)
+        document.getElementById("lesson-" + SELECTEDLESSON).click();
+}
+
+function getRates() {
+    //https://openexchangerates.org/account/usage
+    const apiKey = '11fdd6d5f1274db596c9fb847f5497c2';
+    const apiUrl = `https://open.er-api.com/v6/latest?base=GBP&symbols=USD,EUR&apikey=${apiKey}`;
+
+    // Fetch exchange rates from the API
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const exchangeRatesDiv = document.getElementById('exchangeRates');
+            if (data.result == 'success') {
+                USDRATE = data.rates.USD;
+                EURORATE = data.rates.EUR;
+                console.log(data.rates);
+            } else {
+                console.log("Failed to retrieve exchange rates.", data);
+                USDRATE = 1.27;
+                EURORATE = 1.17;
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching exchange rates:', error);
+            USDRATE = 1.27;
+            EURORATE = 1.17;
+        });
+}
+
+var currencySelector = document.getElementById("currencySelector");
+currencySelector.addEventListener("change", currencyChanged);
+
+getRates();
+// Example usage:
+loadAndHandleJSON('./course.json', setUpLessons);
+
+
+
